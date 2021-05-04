45fcb63cf20a05bfb0e0b8d242f73d0f63ba23f2
franciscamiguel <miguelfrancisca92@gmail.com>
2021-05-04 20:00:30 -0300

    Adicionar arquivos de projeto.

---
+++ DadosDaFran/DadosDaPessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DadosDaFran
+{
+   public class DadosDaPessoa
+    {
+       public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+        public string Nacionalidade { get; set; }
+        public int Telefone { get; set; }
+        public string Agenda{ get; set; }
+        public string Profissao { get; set; }
+
+     
+        
+    }
+}
---
+++ DadosDaFran/MostrarDados.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DadosDaFran
+{
+    public class MostrarDados
+    {
+        public MostrarDados()
+        {
+            var dadosDaPessoa = new DadosDaPessoa
+            {
+                Id = 1,
+                Nome = "Nair Carvalho",
+                Email = "NairdeCarvalho@gamil.com",
+                Idade = 21,
+                Nacionalidade = "Angola",
+                Telefone = 19999999,
+                Agenda = "Nair",
+                Profissao = "Programadora"
+            };
+
+            Imprimir(dadosDaPessoa);
+        }
+
+        public void Imprimir(DadosDaPessoa dadosDaPessoa)
+        {
+            Console.WriteLine("Id da Pessoa: " + dadosDaPessoa.Id);
+            Console.WriteLine("Nome: " + dadosDaPessoa.Nome);
+            Console.WriteLine("Email: " + dadosDaPessoa.Email);
+            Console.WriteLine("Idade: " + dadosDaPessoa.Idade);
+            Console.WriteLine("Nacionalida: " + dadosDaPessoa.Nacionalidade);
+            Console.WriteLine("Telefone: " + dadosDaPessoa.Telefone);
+            Console.WriteLine("Agenda: " + dadosDaPessoa.Agenda);
+            Console.WriteLine("Profissão: " + dadosDaPessoa.Profissao);
+        }
+    }
+}
